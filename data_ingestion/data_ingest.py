1943d942b81ed9665e415dcf36191f788779707f
sunnysavita10 <engsunny3365@gmail.com>
2025-04-26 21:19:47 +0530

    class skeleton created

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -4,3 +4,10 @@ import os
 import pandas as pd
 from data_ingestion.data_transform import data_conveter
 
+
+class ingest_data:
+    def __init__(self):
+        pass
+    
+    def data_ingestion(self):
+        pass
